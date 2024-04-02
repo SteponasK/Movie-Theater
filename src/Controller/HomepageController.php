5d9fc649214e917bfc25a05e364790bcc9b22cbd
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-02 15:23:33 +0300

    created controller skeleton

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace  App\Controller\HomepageController;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomepageController extends AbstractController
+{
+
+}
+
