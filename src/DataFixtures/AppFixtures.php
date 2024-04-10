ab60a73d94cce16a7bb77bf4aaa75b625584199c
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-10 18:56:07 +0300

    Added ficture (movie)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Movie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        # php bin/console doctrine:fixtures:load --append
+        for ($i = 0; $i < 5; ++$i) {
+            $movie = new Movie();
+            $movie->setTitle('MovieTron');
+            $movie->setActors('John Doe, Jane Doe, Jase Doug, Jason Dougs');
+            $movie->setDescription('New epic movie MovieTron about people having fun!!');
+            $movie->setGenre('Horror');
+            $movie->setDirector('Jaser Jason');
+            $movie->setLength('1h 58m');
+            $manager->persist($movie);
+        }
 
         $manager->flush();
     }
 }
+
