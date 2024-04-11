53fc0eabe39e5c3c491c1efa8d6c04e5b211dba5
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-11 17:54:46 +0300

    Updated registrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserController extends AbstractController
 {
